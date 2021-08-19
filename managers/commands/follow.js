03c1670c143a5fc0d41274b8a3dd29952a8092f3
friend <friend@yourfriend.lv>
2021-08-20 01:36:56 +0300

    aah cummy

--- managers/commands/follow.js
+++ managers/commands/follow.js
@@ -3,8 +3,19 @@ export default class command {
 		this.client = client
 	}
 
-	run(message) {
-		this.client.sendMessage(`!! Now following ${message.user.name}!`)
-		this.client.following = message.user.id
+	run(message, args) {
+		if(args.length == 0) {
+			this.client.sendMessage(`!! Now following ${message.user.name}!`)
+			this.client.following = message.user.id
+		} else {
+			if(this.client.users.has(args[0])) {
+				const user = this.client.users.get(args[0])
+				this.client.sendMessage(`!! Now following ${user.name}!`)
+				this.client.following = user.id
+			} else {
+				this.client.sendMessage("!! Couldn't find user!")
+			}
+		}
+
 	}
 }
