c48a2ff0fa19ff9524c2f78c1efe8fae1035cce8
friend <friend@yourfriend.lv>
2021-08-20 18:22:43 +0300

    Add selling

---
+++ managers/commands/sell.js
@@ -0,0 +1,38 @@
+import Josh from "@joshdb/core"
+import provider from "@joshdb/sqlite"
+
+import fs from "fs"
+
+const items = JSON.parse(fs.readFileSync("items.json"))
+
+const USERS = new Josh({
+	name: "MelonBot-Users",
+	provider,
+})
+
+export default class command {
+	constructor(client) {
+		this.client = client
+	}
+
+	async run(message, args) {
+		const me = await USERS.get(message.user._id)
+		const itemObject = items.find(e => e.name == args[0])
+		
+		if(!itemObject) {
+			this.client.sendMessage("!! Item doesn't exist.")
+		} else {
+			const item = me.inventory.find(e => e.id == itemObject.id)
+
+			if(item) {
+				this.client.sendMessage("!! Sold " + item.amount + " for " + (itemObject.cost * item.amount))
+				
+				me.money += itemObject.cost * item.amount
+				
+				await USERS.set(message.user._id, me)
+			} else {
+				this.client.sendMessage("!! You don't have this item.")
+			}
+		}
+	}
+}
